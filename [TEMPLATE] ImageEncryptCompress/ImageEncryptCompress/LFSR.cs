a9128f672bc2e67f28c8c6aaa2e6c2379cd19263
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-11-29 22:39:58 +0200

    init commit

---
+++ [TEMPLATE] ImageEncryptCompress/ImageEncryptCompress/LFSR.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageQuantization
+{
+    class LFSR
+    {
+        bool[] bits;
+
+        public LFSR(int bitCount, string seed)
+        {
+            bits = new bool[bitCount];
+
+            for (int i = 0; i < bitCount; i++)
+                bits[i] = seed[i] == '1' ? true : false;
+
+        }
+
+        public string Registry
+        {
+            get
+            {
+                char[] t = new char[bits.Length];
+                for (int i = 0; i < bits.Length; i++)
+                    t[i] = bits[i] ? '1' : '0';
+
+                return new string(t);
+            }
+        }
+
+        public void Shift()
+        {
+            bool bnew = (bits[bits.Length - 1] ^ bits[bits.Length - 2]);
+
+            for (int i = bits.Length - 1; i > 0; i--)
+            {
+                bits[i] = bits[i - 1];
+            }
+            bits[0] = bnew;
+        }
+    }
+}
---
+++ [TEMPLATE] ImageEncryptCompress/ImageEncryptCompress/MainForm.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageQuantization
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        RGBPixel[,] ImageMatrix, EncryptionImageMatrix;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open the browsed image and display it
+                string OpenedFilePath = openFileDialog1.FileName;
+                ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
+                ImageOperations.DisplayImage(ImageMatrix, pictureBox1);
+            }
+            txtWidth.Text = ImageOperations.GetWidth(ImageMatrix).ToString();
+            txtHeight.Text = ImageOperations.GetHeight(ImageMatrix).ToString();
+        }
+
+        private void btnGaussSmooth_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                EncryptionImageMatrix = ImageOperations.EncryptImage(ImageMatrix);
+            }
+            ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
+
+          //  double sigma = double.Parse(txtGaussSigma.Text);
+        //    int maskSize = (int)nudMaskSize.Value;
+         //   ImageMatrix = ImageOperations.GaussianFilter1D(ImageMatrix, maskSize, sigma);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EncryptionImageMatrix = ImageOperations.EncryptImage(EncryptionImageMatrix);
+            ImageOperations.DisplayImage(EncryptionImageMatrix, pictureBox2);
+        }
+    }
+}
